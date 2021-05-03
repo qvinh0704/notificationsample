77cff5f72133e994f83e1a157d8a1fdf19b1d68b
Vinh Le <vinh.lq@vdsc.com.vn>
2021-05-03 22:43:48 +0700

    Update demo

--- App.js
+++ App.js
@@ -8,15 +8,7 @@
 
 import React from 'react';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {TextInput, Button, View, TouchableOpacity, Text} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
@@ -25,40 +17,21 @@ import OneSignal from 'react-native-onesignal';
 import {Notifications} from 'react-native-notifications';
 
 const App: () => Node = () => {
+  const [externalId, setExternalId] = React.useState('');
+  const [text, setText] = React.useState('');
+
   React.useEffect(() => {
     OneSignal.setAppId('16394537-23d0-4405-827a-ae3a3e6bd6d4');
-    OneSignal.setExternalUserId('A1234');
-
-    Notifications.android.setNotificationChannel({
-      channelId: '6a381483-bf50-4e13-824b-7a1f546971e0',
-      name: 'Sport newssssss',
-    });
-
-    console.log(
-      'setNotificationChannel',
-      Notifications.setNotificationChannel({
-        channelId: '6a381483-bf50-4e13-824b-7a1f546971e0',
-        name: 'Sport newssssss',
-      }),
-    );
-
-    Notifications.postLocalNotification({
-      body: 'Local notification!',
-      title: 'Local Notification Title',
-      userInfo: {},
-    });
 
     OneSignal.setNotificationWillShowInForegroundHandler(notifReceivedEvent => {
+      const notification = notifReceivedEvent.getNotification();
+      const privateBody = notification?.additionalData?.private_body;
+
       Notifications.postLocalNotification({
-        body: 'Local notification!',
-        title: 'Local Notification Title',
-        userInfo: {},
+        body: privateBody || notification?.body,
+        title: notification?.title || '',
       });
-      notifReceivedEvent.complete(notifReceivedEvent.getNotification());
-      // const notification = notifReceivedEvent.getNotification();
-      // const privateBody = notification?.additionalData?.private_body;
-      // console.log('notifReceivedEvent', notifReceivedEvent);
-      // console.log('privateBody', privateBody);
+      notifReceivedEvent.complete(null);
     });
     OneSignal.setNotificationOpenedHandler(openedEvent => {
       console.log('OneSignal: notification opened:', openedEvent);
@@ -81,7 +54,73 @@ const App: () => Node = () => {
     OneSignal.getDeviceState().then(res => console.log('DeviceState', res));
   }, []);
 
-  return <View flex={1} style={{backgroundColor: 'blue'}}></View>;
+  React.useEffect(() => {
+    if (externalId) {
+      OneSignal.setExternalUserId(externalId);
+    } else {
+      OneSignal.removeExternalUserId();
+    }
+  }, [externalId]);
+
+  return (
+    <View
+      flex={1}
+      style={{
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <TextInput
+        placeholder="Nhập id"
+        style={{
+          paddingHorizontal: 8,
+          width: '50%',
+          height: 50,
+          borderRadius: 4,
+          borderWidth: 0.8,
+          borderColor: 'gray',
+          marginBottom: 32,
+        }}
+        onChangeText={value => setText(value)}
+      />
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          paddingHorizontal: 16,
+          width: '100%',
+        }}>
+        <TouchableOpacity
+          onPress={() => setExternalId(text)}
+          style={{
+            padding: 8,
+            paddingHorizontal: 16,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#87ceeb',
+            borderRadius: 8,
+          }}>
+          <Text>Đăng nhập với Id</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            padding: 8,
+            paddingHorizontal: 16,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'salmon',
+            borderRadius: 8,
+          }}
+          onPress={() => {
+            setText('');
+            setExternalId('');
+          }}>
+          <Text style={{color: 'white'}}>Đăng xuất</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
 
 export default App;
